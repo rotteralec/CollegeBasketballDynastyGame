30c730845df048723f1fe24c7b4b3fe749105b00
rotteralec <rotteralec@gmail.com>
2024-05-30 16:54:00 -0400

    Basic School class generator skeleton

--- School.py
+++ School.py
@@ -1,15 +1,28 @@
 from Team import *
 
+
+
+def GenerateHistory():
+    return {}
+
+def GenerateStats():
+    return {}
+
+def GenerateBoosters():
+    return []
+def GenerateSchedule():
+    return []
+
 class School:
 
     def __init__(self, team, coach):
         self.team = team
         self.coach = coach
-        self.history = {}
+        self.history = GenerateHistory()
         self.rival = "Michigan"
         self.level = 50
         self.prestige = 5
-        self.stats = {}
+        self.stats = GenerateStats()
         self.location = "East Lansing, MI"
         self.boosters = []
         self.schedule = []
