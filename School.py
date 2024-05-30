4547c8ca4a45ee9f3e993ef437f2beabb01e04cc
rotteralec <rotteralec@gmail.com>
2024-05-30 16:46:26 -0400

    Added school class

---
+++ School.py
@@ -0,0 +1,15 @@
+from Team import *
+
+class School:
+
+    def __init__(self, team, coach):
+        self.team = team
+        self.coach = coach
+        self.history = {}
+        self.rival = "Michigan"
+        self.level = 50
+        self.prestige = 5
+        self.stats = {}
+        self.location = "East Lansing, MI"
+        self.boosters = []
+        self.schedule = []
