ba12c7a27ed4db4c94b327c6570539679bddabc8
rotteralec <rotteralec@gmail.com>
2024-06-12 13:31:42 -0400

    added basic season and recruit init

--- Recruit.py
+++ Recruit.py
@@ -1,4 +1,16 @@
 
+##Needs vague stat potentials AND recruit priorities
+
+##Priorities: Proximity to home, School Prestige, Stadium Atmos, Facilities, College Life, Academics
+#cont#Coach prestige, getting to the league, 
+
+class Recruit:
 
 
-class Recruit
+    def __init__(self):
+        self.id = 0
+        self.fname = "Test"
+        self.lname = "Test"
+        self.pos = "PG"
+        self.priorities = []
+        self.stats = []
--- Season.py
+++ Season.py
@@ -2,10 +2,12 @@ import json
 import random
 
 
+##Seasons recruiting class (Starting with only seniors then look at expanding to juco/transfers at end of year and multi year recruiting)
+
 ##15 weeks 30 regular season games or just 30 sections?
 #Conference is one more week or up to 5 or 6 with new schools
 #national tournament 6 games (or nit/cbi maybe???)
-##Offseason: Players leaving, Players transferring, Draft, Offseason recruiting
+##Offseason: Players leaving, Players transferring, Draft, Offseason recruiting 1 2 3
 
 noncon = []
 con = []
@@ -13,4 +15,14 @@ confTour = []
 natTour = []
 
 
-
+##gameday [game, HC RAL, OC RAL, DC RAL, Depth chart, Gameplan, practice]
+def genGameday(_game):
+    temp_arr = []
+    temp_arr.append(_game)
+    temp_arr.append([])
+    temp_arr.append([])
+    temp_arr.append([])
+    temp_arr.append(False)
+    temp_arr.append(False)
+    temp_arr.append(False)
+    return temp_arr
